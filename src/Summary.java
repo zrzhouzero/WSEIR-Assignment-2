3c70eadf0abd7cb141ab11d34ce2cb14ad985caf
Zero <zrzhouzero@gmail.com>
2019-10-07 23:25:49 +1100

    created new files

---
+++ src/Summary.java
@@ -0,0 +1,22 @@
+import util.DocumentRankingPoints;
+
+import java.util.ArrayList;
+
+public class Summary {
+
+    private int documentId;
+    private static final String directoryPath = "docs/";
+
+    public Summary(int documentId) {
+        this.documentId = documentId;
+    }
+
+    public void generateStaticSummary() {
+
+    }
+
+    public void generateDynamicSummary() {
+
+    }
+
+}
