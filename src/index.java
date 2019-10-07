7682a76c11b2adf79eb9d036df8299e89fcad401
Zero <zrzhouzero@gmail.com>
2019-10-08 02:10:01 +1100

    updated dynamic summary function

--- src/index.java
+++ src/index.java
@@ -78,7 +78,7 @@ public class index {
                     for (String word : words) {
                         matcher = pattern.matcher(word);
                         word = matcher.replaceAll("").toLowerCase().trim();
-                        if(word.equals("")) continue;
+                        if (word.equals("")) continue;
                         if (stoplist.contains(word)) continue;
                         System.out.println(word);
                         count += word.length();
@@ -88,15 +88,15 @@ public class index {
             }
             new Thread(new Runnable() {
 
-				@Override
-				public void run() {
-		            try {
-						writeInvlistToFile();
-					} catch (IOException e) {
-						e.printStackTrace();
-					}					
-				}
-            	
+                @Override
+                public void run() {
+                    try {
+                        writeInvlistToFile();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+
             }).start();
             mapWriter.close();
             reader.close();
--- src/util/MinHeap.java
+++ src/util/MinHeap.java
@@ -4,10 +4,23 @@ import java.util.ArrayList;
 
 public interface MinHeap {
 
+    /**
+     * add a document ID, document ranking point pair into the min heap
+     *
+     * @param pair the inserting pair
+     */
     void addElement(DocumentRankingPoints pair);
 
+    /**
+     * print the ranking result
+     */
     void printRank();
 
+    /**
+     * output the ranking result as a list
+     *
+     * @return the ranking result list
+     */
     ArrayList<DocumentRankingPoints> outputRank();
 
 }
